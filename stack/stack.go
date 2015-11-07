3b4125b8c0fdd4bc5c0ee0ad9634dd7f7f3cc381
Albert Hermida <alberthermida@gmail.com>
2015-11-07 10:43:45 -0500

    added structure for stack

---
+++ stack/stack.go
@@ -0,0 +1,42 @@
+/**
+  An implementation of the Stack ADT
+*/
+package stack
+
+import "sync"
+
+/**
+  Define Struct of type Stack, use lock to make temporarily immutable on changes.
+  Using an interflace slice to represent stack because they're not forced
+  to a particular type.
+*/
+type Stack struct {
+  stack  []interface{}
+  length int
+  lock   sync.Mutex
+}
+
+/**
+  Stack constructor
+*/
+func NewStack() *Stack {
+  s := &Stack{}
+  s.stack = make([]interface, 0)
+  s.length = 0
+  return s
+}
+
+/**
+  Flesh out the Stack struct
+*/
+func (s *Stack) Push(){
+
+}
+
+func (s *Stack) Pop(){
+
+}
+
+func (s *Stack) Length(){
+
+}
